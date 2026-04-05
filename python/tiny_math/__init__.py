c45c4c9ff6e5e2b234b4a5252980aaed0098cd7a
ishikawash <ishikawash@gmail.com>
2026-04-05 20:25:57 +0900

    Add files to copy.

---
+++ python/tiny_math/__init__.py
@@ -0,0 +1,11 @@
+from ._tiny_math import (
+    __version__,
+    ComplexNumber,
+    exponent
+)
+
+__all__ = [
+    "__version__",
+    "ComplexNumber",
+    "exponent"
+]
---
+++ python/tiny_math/__init__.pyi
@@ -0,0 +1,6 @@
+from __future__ import annotations
+from tiny_math._tiny_math import ComplexNumber
+from tiny_math._tiny_math import exponent
+from . import _tiny_math
+__all__: list = ['__version__', 'ComplexNumber', 'exponent']
+__version__: str = '0.1.0'
